c047b846a73e11e987cd3e74716490650ae2e457
cnctfd194 <lamborghinimaniac_2@hotmail.com>
2016-06-10 23:00:47 +0800

    Update extension.js

--- extension.js
+++ extension.js
@@ -38,42 +38,6 @@
           }
         };
 
-        bot.commands.cakeCommand: = {
-           command: 'cake',
-           rank: 'user',
-           type: 'startsWith',
-           getCake: function (chat) {
-             var c = Math.floor(Math.random() * basicBot.chat.cake.length);
-             return basicBot.chat.cake[c];
-         },
-           functionality: function (chat, cmd) {
-             if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
-             if (!basicBot.commands.executable(this.rank, chat)) return void (0);
-             else {
-                 var msg = chat.message;
- 
-                 var space = msg.indexOf(' ');
-                 if (space === -1) {
-                     API.sendChat(basicBot.chat.eatcake);
-                     return false;
-                 }
-                 else {
-                     var name = msg.substring(space + 2);
-                     var user = basicBot.userUtilities.lookupUserName(name);
-                     if (user === false || !user.inRoom) {
-                         return API.sendChat(subChat(basicBot.chat.nousercake, {name: name}));
-                     }
-                     else if (user.username === chat.un) {
-                         return API.sendChat(subChat(basicBot.chat.selfcake, {name: name}));
-                     }
-                     else {
-                         return API.sendChat(subChat(basicBot.chat.cake, {nameto: user.username, namefrom: chat.un, cake: this.getCake()}));    }
-                     }
-                 }
-             }
-           }
-         };
- 
         // Load the chat package again to account for any changes
         bot.loadChat();
 
